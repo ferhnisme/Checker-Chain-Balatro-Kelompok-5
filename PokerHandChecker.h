e5d6667454b4c334b01d942b395baf3837cfccd7
Doni <gakniat15@gmail.com>
2026-04-03 00:48:52 +0700

    doni_PHC.h
    Pocker Hand Checker

---
+++ PokerHandChecker.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Hand.h"
+enum class HandRank {
+HIGH_CARD,
+PAIR,
+TWO_PAIR,
+THREE_OF_A_KIND,
+STRAIGHT,
+FLUSH,
+FULL_HOUSE
+};
+class PokerHandChecker{
+public:
+virtual HandRank check(const Hand& hand) = 0;
+void setNext(PokerHandChecker* next);
+protected:
+PokerHandChecker* nextChecker = nullptr;
+};
+
+#include "PokerHandChecker.h"
+void PokerHandChecker::setNext(PokerHandChecker* next){
+nextChecker = next;
+}
