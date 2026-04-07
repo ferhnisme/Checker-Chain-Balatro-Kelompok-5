ad527ad8d16c92f962fd6b869da38eb81a952342
Keiven Cicero Nathaniel <200770710+keivencicero@users.noreply.github.com>
2026-04-07 22:56:41 +0700

    Update ScoringRule.cpp

--- Code/ScoringRule.cpp
+++ Code/ScoringRule.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "ScoringRule.h"
 ScoringRule::ScoringRule(){
+flushFiveChecker.setNext(&fiveOfAKindChecker);
+fiveOfAKindChecker.setNext(&royalFlushChecker);
+royalFlushChecker.setNext(&straightFlushChecker);
+straightFlushChecker.setNext(&fourOfAKindChecker);
+fourOfAKindChecker.setNext(&fullHouseChecker);
 fullHouseChecker.setNext(&flushChecker);
-flushChecker.setNext(&fourOfAKindChecker);
-fourOfAKindChecker.setNext(&straightChecker);
+flushChecker.setNext(&straightChecker);
 straightChecker.setNext(&threeOfAKindChecker);
 threeOfAKindChecker.setNext(&twoPairChecker);
 twoPairChecker.setNext(&pairChecker);
@@ -11,7 +15,7 @@ pairChecker.setNext(&highCardChecker);
 }
 int ScoringRule::scoreHand(const Hand& hand){
 std::cout << "Calculating hand score...\n";
-HandRank rank = fullHouseChecker.check(hand);
+HandRank rank = flushFiveChecker.check(hand); 
 int score = convertRankToScore(rank);
 std::cout << "Final score = " << score << "\n";
 return score;
@@ -35,5 +39,13 @@ case HandRank::STRAIGHT:
     return 22;
 case HandRank::FOUR_OF_A_KIND:
     return 30;    
+case HandRank::STRAIGHT_FLUSH:
+    return 50;
+case HandRank::ROYAL_FLUSH:
+    return 100;
+case HandRank::FIVE_OF_A_KIND:
+    return 120;
+case HandRank::FLUSH_FIVE:
+    return 150;
 }
 }
